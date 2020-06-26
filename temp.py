a71f680f6509398f52184fa366069760ad3008f8
Juno020825 <67449137+Juno020825@users.noreply.github.com>
2020-06-26 11:41:39 +0900

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,16 @@
+html = b"""
+<html>
+    <body>
+        <form method = "get" action="">
+        a = <input type="number" name="a" ><br>
+        b = <input type="number" name="b"><br>
+
+            <input type="submit">
+
+        </form>
+        sum = %(sum)s <br><br>
+        mul = %(mul)s
+
+    </body>
+</html>
+"""
